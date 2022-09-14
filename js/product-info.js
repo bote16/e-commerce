95c27315d7f15b2e7df8714de963e2bca5bb8976
bote16 <jbotejara@gmail.com>
2022-09-14 13:00:48 -0300

    Entrega 3 parte 2
    Añadir producto segun ID on click

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,39 @@
 let productID = localStorage.getItem("productID");
 
-
 document.addEventListener("DOMContentLoaded", async function () {
+  const prodPrice = document.querySelector("#prodPrice");
+  const prodSoldCount = document.querySelector("#prodSoldCount");
+  const prodDescription = document.querySelector("#prodDescription");
+  const prodCategory = document.querySelector("#prodCategory");
+  const divImageProd = document.querySelector("#image-cnt");
+  const prodName = document.querySelector("#prodName");
 
-})
+  let res = await getJSONData(PRODUCT_INFO_URL + productID + ".json");
+  if (res.status === "ok") {
+    product = res.data;
+    product_images_array = product.images;
+    showProductPhotos();
+  }
+
+  prodName.innerText = product.name;
+  product.currency + " " + product.cost;
+  prodSoldCount.innerText = product.soldCount;
+  prodDescription.innerText = product.description;
+  prodCategory.innerText = product.category;
+
+  function showProductPhotos() {
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < product_images_array.length; i++) {
+      let image = product_images_array[i];
+
+      htmlContentToAppend += `
+      <div class="row p-2">  
+          <img src="${image}" alt="${product.description}" class="img-thumbnail">
+      </div>
+  `;
+    }
+
+    divImageProd.innerHTML = htmlContentToAppend;
+  }
+});
