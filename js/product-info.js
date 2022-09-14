c212ff58a694141c2a8d07f7ad0ae832a3769fea
bote16 <jbotejara@gmail.com>
2022-09-14 16:56:42 -0300

    Entrega 3 parte 3
    Falta agregar el fill con el score del comment

--- js/product-info.js
+++ js/product-info.js
@@ -7,12 +7,21 @@ document.addEventListener("DOMContentLoaded", async function () {
   const prodCategory = document.querySelector("#prodCategory");
   const divImageProd = document.querySelector("#image-cnt");
   const prodName = document.querySelector("#prodName");
+  const cntComments = document.querySelector("#comments-container");
 
   let res = await getJSONData(PRODUCT_INFO_URL + productID + ".json");
   if (res.status === "ok") {
     product = res.data;
     product_images_array = product.images;
     showProductPhotos();
+  }
+
+  let res_comment = await getJSONData(
+    PRODUCT_INFO_COMMENTS_URL + productID + ".json"
+  );
+  if (res_comment.status === "ok") {
+    comments = res_comment.data; //array --
+    showCommentInfo();
   }
 
   prodName.innerText = product.name;
@@ -35,5 +44,52 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
 
     divImageProd.innerHTML = htmlContentToAppend;
+  }
+
+  function showCommentInfo() {
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < comments.length; i++) {
+      let comment = comments[i];
+
+      htmlContentToAppend += `
+      <div class="p-2 mt-2 overflow-hidden shadow rounded cursor-active">
+            <div class="row">
+              <div class="col">
+                <p class="fst-italic fw-bold">${comment.user} - ${comment.dateTime}</p>
+              </div>
+            </div>
+            <div class="row">
+              <p class="fst-italic">${comment.description}</p>
+            </div>
+            <div class="row">
+              <p>
+                Calificación <span class="fa fa-star stars"></span>
+                <span class="fa fa-star stars"></span>
+                <span class="fa fa-star stars"></span>
+                <span class="fa fa-star stars"></span>
+                <span class="fa fa-star stars"></span>
+              </p>
+            </div>
+          </div>
+  `;
+    }
+    cntComments.innerHTML += htmlContentToAppend;
+  }
+
+  function fillStars() {
+    //esto sirve para la parte 4, cuando el usuario clickea que haga fill o quite. Crear queryselector con clase fa-stars, la otra se usa para el fetch de comment
+    stars.forEach((star, i) => {
+      star.onclick = function () {
+        let current_star = i + 1;
+        stars.forEach((star, j) => {
+          if (current_star >= j + 1) {
+            star.classList.add("checked");
+          } else {
+            star.classList.remove("checked");
+          }
+        });
+      };
+    });
   }
 });
